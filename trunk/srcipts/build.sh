dc6257e1b8e575b245ff7dc30928697d51f32c52
marcin.slowik@gmail.com <marcin.slowik@gmail.com@ffcfd6bb-e687-70df-aad9-753e6a0b50a6>
2012-08-30 18:05:41 +0000

    Buuld script fixed

--- trunk/srcipts/build.sh
+++ trunk/srcipts/build.sh
@@ -7,9 +7,9 @@ mkdir -p usr/lib/enigma2/python/Plugins/Extensions/Filmweb
 cp -R ../src/* usr/lib/enigma2/python/Plugins/Extensions/Filmweb
 rm -rf `find . -type d -name .svn`
 rm -rf `find . -type f -name Filmweb.po`
-tar zcvf data.tar.gz usr
+tar zcvf data.tar.gz ./usr
 cd ../srcipts/control
-tar zcvf ../../tmp/control.tar.gz *
+tar zcvf ../../tmp/control.tar.gz ./*
 cd ../../tmp
 cp ../srcipts/debian-binary ./
 ar -r ../enigma2-plugin-extensions-filmweb_1.0_all.ipk ./debian-binary ./data.tar.gz ./control.tar.gz
